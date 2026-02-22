304b9ec4b9f14462ba00e9d317c5a38ec86fe77e
Ivina Lorena Oliveira Moura <ivinalorena@aluno.unilab.edu.br>
2026-02-22 20:39:30 -0300

    inicio do crud

--- pages/PAGELSTM.py
+++ pages/PAGELSTM.py
@@ -12,8 +12,11 @@ st.divider()
 
 st.markdown("# Modelo escolhido: RealMLP")
 st.markdown("""###  Informações relevantes sobre o modelo escolhido na aba atual: 
-            As redes Long Short-Term Memory (LSTMs) constituem uma classe de Redes Neurais Recorrentes (RNNs) capazes de capturar e aprender dependências temporais de longo alcance. 
-            fundamentam-se em células de memória e um conjunto de mecanismos de controle, denominados 'gates', que regulam o fluxo de informação. Através dos gates de entrada, 
-            esquecimento e saída, a LSTM adquire a capacidade de selecionar quais informações devem ser armazenadas, atualizadas ou descartadas ao longo do tempo. Este controle 
-            seletivo permite a retenção de dependências temporais prolongadas, tornando a LSTM uma arquitetura de rede neural recorrente particularmente adequada para tarefas como 
-            processamento de linguagem natural, previsão de séries temporais e reconhecimento de padrões em dados sequenciais.""")
+As redes Long Short-Term Memory (LSTMs) constituem uma classe de Redes Neurais Recorrentes (RNNs) capazes de capturar e aprender dependências temporais de longo alcance. 
+            
+- Fundamentam-se em células de memória e um conjunto de mecanismos de controle, denominados 'gates', que regulam o fluxo de informação. Através dos gates de entrada, 
+esquecimento e saída, a LSTM adquire a capacidade de selecionar quais informações devem ser armazenadas, atualizadas ou descartadas ao longo do tempo. Este controle 
+seletivo permite a retenção de dependências temporais prolongadas, tornando a LSTM uma arquitetura de rede neural recorrente particularmente 
+- Adequada para tarefas como: 
+    - processamento de linguagem natural, 
+    - previsão de séries temporais e reconhecimento de padrões em dados sequenciais.""")
--- pages/PAGEMLP.py
+++ pages/PAGEMLP.py
@@ -17,3 +17,16 @@ Real valued Multi-Layer Perceptron - é um modelo de deep learning avançado, pr
 - é um algoritmo de aprendizado supervisionado que aprende uma função *f:R^m -> R^0* treinando em um conjunto de dados, onde o m é o número de dimensões para entrada e é o O número de dimensões para saída.
 - Dado um conjunto de características X = {x1,x2,...,xm} e um alvo y, ele pode aprender um aproximador de função não linear para classificação ou regressão.
 - É diferente da regressão logística, pois entre a camada de entrada e a de saída, pode haver uma ou mais camadas não lineares, chamadas camadas ocultas. """) #,height=200, key="system_prompt")
+
+with st.form(key='form_MLP', border=True):
+    nome = st.text_input('Nome')
+    idade = st.number_input('Idade',format='%d',max_value=100,value=None)
+    file = st.file_uploader('Insira os dados do paciente',accept_multiple_files=False,type="csv")
+    
+    if file is not None: 
+        st.dataframe(file)
+
+    if nome or idade or file is None:
+        st.warning('Complete o cadastro')
+    st.form_submit_button('Cadastrar')
+
